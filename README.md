4541cfb9bf1b41d07183de55d8a7abe79490a7ce
Sergey Alikin <salikin@syntellect.ru>
2020-08-13 16:14:17 +0300

    Project.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SqlDataClientStringPerformanceIssue
+Check connection string in SqlDataClientStringPerformanceIssue/Program.cs prior to running app.
+
+Program generates ~ 10 MB string and stores it into nvarchar(max) column (table is created if not exists), then it attempts to select it using ExecuteScalarAsync.
+
+Method ExecuteScalarAsync is executing up to 10 minutes, probably because of DataReader creating multiple byte arrays for each portion of data received from SQL. There are lots of CPU consumption and GCs when executing the query.
+
+Selecting text in SSMS occures in less than a second.
---
+++ SqlDataClientStringPerformanceIssue/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace SqlDataClientStringPerformanceIssue
+{
+    public static class Program
+    {
+        public async static Task Main()
+        {
+            // generating ~ 10 MB of text
+            string text = LoremIpsum(15, 100_000);
+            await Console.Out.WriteLineAsync("Text length: " + text.Length.ToString("N0", CultureInfo.InvariantCulture));
+
+            await using var connection = SqlClientFactory.Instance.CreateConnection() ?? throw new InvalidOperationException("No connection");
+            connection.ConnectionString = "Server=.;Database=master;User ID=sa;Password=Master1234";
+
+            await connection.OpenAsync();
+
+            // drop table LargeText if exists
+            {
+                await using var command = connection.CreateCommand();
+                command.CommandText = "IF EXISTS (SELECT * FROM [sysobjects] WHERE [name]='LargeText' AND [xtype]='U') DROP TABLE [LargeText]";
+                await command.ExecuteNonQueryAsync();
+            }
+
+            // create table LargeText
+            {
+                await using var command = connection.CreateCommand();
+                command.CommandText = "CREATE TABLE [LargeText] ([ID] uniqueidentifier NOT NULL PRIMARY KEY CLUSTERED, [Text] nvarchar(max) NULL)";
+                await command.ExecuteNonQueryAsync();
+            }
+
+            // insert generated text into LargeText
+            {
+                await using var command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO [LargeText] ([ID], [Text]) VALUES (NEWID(), @Text)";
+
+                DbParameter textParameter = command.CreateParameter();
+                textParameter.ParameterName = "Text";
+                textParameter.DbType = DbType.String;
+                textParameter.Value = text;
+                command.Parameters.Add(textParameter);
+
+                await command.ExecuteNonQueryAsync();
+            }
+
+            // select inserted text and measure time
+            await using var selectCommand = connection.CreateCommand();
+            selectCommand.CommandText = "SELECT [Text] FROM [LargeText]";
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string selectedText = await selectCommand.ExecuteScalarAsync() as string;
+            stopwatch.Stop();
+
+            await Console.Out.WriteLineAsync("Selected text length: " + (selectedText?.Length ?? -1).ToString("N0", CultureInfo.InvariantCulture));
+            await Console.Out.WriteLineAsync("Elapsed: " + stopwatch.Elapsed);
+        }
+
+        private static string LoremIpsum(int numWords, int numSentences)
+        {
+            var words = new[]
+            {
+                "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
+                "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
+                "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"
+            };
+
+            var random = new Random();
+            var result = new StringBuilder();
+
+            for (int i = 0; i < numSentences; i++)
+            {
+                for (int j = 0; j < numWords; j++)
+                {
+                    if (j > 0)
+                    {
+                        result.Append(' ');
+                    }
+
+                    result.Append(words[random.Next(words.Length)]);
+                }
+
+                result.Append(". ");
+            }
+
+            return result.ToString();
+        }
+    }
+}
